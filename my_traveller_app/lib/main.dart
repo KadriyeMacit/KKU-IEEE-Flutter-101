6e7069f8325119c39aa97a340c981ba24e435ade
Kadriye <kadriyemacit9655@gmail.com>
2021-05-23 15:21:12 +0300

    drawer eklendi

--- my_traveller_app/lib/main.dart
+++ my_traveller_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
       home: LaunchPage(),
     );
--- my_traveller_app/lib/views/menu_page.dart
+++ my_traveller_app/lib/views/menu_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_traveller_app/views/favorite_page.dart';
 import 'package:my_traveller_app/views/home_page.dart';
+import 'package:my_traveller_app/views/launch_page.dart';
 import 'package:my_traveller_app/views/profile_page.dart';
 
 class MenuPage extends StatefulWidget {
@@ -30,6 +31,43 @@ class _MenuPageState extends State<MenuPage> {
           title: Text(
             'Gezi Rehberim',
             style: TextStyle(color: Colors.black),
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text(
+                  'Kadriye',
+                  style: TextStyle(color: Colors.white),
+                ),
+                accountEmail: Text(
+                  'kadriye@gmail.com',
+                  style: TextStyle(color: Colors.white),
+                ),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/avatar.png'),
+                ),
+              ),
+              ListTile(
+                  title: Text('Anasayfa'),
+                  leading: Icon(Icons.home),
+                  onTap: () => Navigator.pop(context)),
+              ListTile(
+                title: Text('Ayarlar'),
+                leading: Icon(Icons.settings),
+              ),
+              Divider(),
+              ListTile(
+                  title: Text('Çıkış yap'),
+                  leading: Icon(Icons.logout),
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LaunchPage(),
+                      )))
+            ],
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
